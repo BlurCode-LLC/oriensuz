f09da8d3396f3110598e60fbcd7a1eae7ab9db7b
abrornomos <abrornomos2018@gmail.com>
2022-04-20 01:28:10 +0500

    Changes 19.04.22

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:8001'
 timeout = 3600
 accesslog = 'log/access.log'
 errorlog = 'log/error.log'
